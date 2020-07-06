39cc77b68605101437a3672c930cc7731dfb6e37
Labais <reinitto@gmail.com>
2020-07-06 16:50:13 +0200

    add favicon

--- src/components/ExternalButton.js
+++ src/components/ExternalButton.js
@@ -1,5 +1,4 @@
 import React from "react";
-import externalIcon from "../icons8-external-link.svg";
 export let ExternalButton = ({ link, text }) => {
   let openNewTab = (link) => {
     window.open(link, "_blank");
@@ -10,14 +9,6 @@ export let ExternalButton = ({ link, text }) => {
         position: "relative",
       }}
     >
-      <img
-        style={{
-          position: "absolute",
-          width: "14px",
-        }}
-        src={externalIcon}
-        alt="external link icon"
-      />
       <button className={`nav-item`} onClick={() => openNewTab(link)}>
         <p>{text}</p>
       </button>
